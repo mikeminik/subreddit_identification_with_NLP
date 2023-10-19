1f385350a731d46f50cb530e9aec60a3c47cfb33
Mike Minikowski <mike.minikowski@gmail.com>
2023-10-18 23:07:23 -0400

    first model finished

--- code/subreddit_data_retrieval.py
+++ code/subreddit_data_retrieval.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[77]:
+
+
 import praw
 import pandas as pd
 import datetime
 
+config = pd.read_json('../config.json', typ='series')
+
 reddit = praw.Reddit(
-    client_id='V1HfcNB5ea0meMmhbOqe5A',
-    client_secret='gbREmHM3-qwDNEbN5t5s-YuI6q-JEA',
-    user_agent='py_dsi_praw',
-    username='py_dsi',
-    password='dsir-0911'
+    client_id=config['client_id'],
+    client_secret=config['client_secret'],
+    user_agent=config['user_agent'],
+    username=config['username'],
+    password=config['password']
 )
 
 ## Futurology
@@ -28,17 +36,20 @@ listings = [(hot_posts, 'hot'),
 subreddit_1_data = []
 for listing, label in listings:
     for post in listing:
-        subreddit_1_data.append([label, post.id, post.created_utc, post.title, post.selftext, post.subreddit, post.upvote_ratio])
+        subreddit_1_data.append([label, post.id, post.created_utc, post.title, post.selftext, post.subreddit, post.upvote_ratio, post.url, post.author])
 
-futurology = pd.DataFrame(subreddit_1_data, columns = ['type', 'id','created_utc', 'title', 'self_text', 'subreddit', 'upvote_ratio'])
+futurology = pd.DataFrame(subreddit_1_data, columns = ['type', 'id','created_utc', 'title', 'self_text', 'subreddit', 'upvote_ratio', 'link_address', 'user'])
+
 futurology['datetime'] = futurology['created_utc'].apply(lambda time: datetime.datetime.fromtimestamp(time))
+
 futurology.drop('created_utc', axis = 1, inplace = True)
+
 futurology_uniques = futurology.drop_duplicates(subset='id')
+
 futurology_uniques.set_index('id', inplace = True)
 
-futurology_uniques.to_csv('../datasets/futurology_' + str(datetime.date.today())+ '.csv', index=True)
 
-## Scifi
+futurology_uniques.to_csv('../datasets/futurology_' + str(datetime.date.today())+ '_with_links.csv', index=True)
 
 subreddit_2 = reddit.subreddit('scifi')
 
@@ -56,12 +67,17 @@ listings = [(hot_posts, 'hot'),
 subreddit_2_data = []
 for listing, label in listings:
     for post in listing:
-        subreddit_2_data.append([label, post.id, post.created_utc, post.title, post.selftext, post.subreddit, post.upvote_ratio])
+        subreddit_2_data.append([label, post.id, post.created_utc, post.title, post.selftext, post.subreddit, post.upvote_ratio, post.url, post.author])
 
-scifi = pd.DataFrame(subreddit_2_data, columns = ['type', 'id','created_utc', 'title', 'self_text', 'subreddit', 'upvote_ratio'])
+scifi = pd.DataFrame(subreddit_2_data, columns = ['type', 'id','created_utc', 'title', 'self_text', 'subreddit', 'upvote_ratio', 'link_address', 'user'])
+
 scifi['datetime'] = scifi['created_utc'].apply(lambda time: datetime.datetime.fromtimestamp(time))
+
 scifi.drop('created_utc', axis = 1, inplace = True)
+
 scifi_uniques = scifi.drop_duplicates(subset='id')
+
 scifi_uniques.set_index('id', inplace = True)
 
-scifi_uniques.to_csv('../datasets/scifi_' + str(datetime.date.today())+ '.csv', index=True)
+scifi_uniques.to_csv('../datasets/scifi_' + str(datetime.date.today())+ '_with_links.csv', index=True)
+
